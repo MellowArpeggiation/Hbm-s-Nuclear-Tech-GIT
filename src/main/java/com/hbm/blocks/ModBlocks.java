cb52b8930c54a8f242fad3c82f62d43117c02c33
StarlarkXVII <118313105+StarlarkXVII@users.noreply.github.com>
2024-11-24 19:52:04 +0000

    Update ModBlocks.java

--- src/main/java/com/hbm/blocks/ModBlocks.java
+++ src/main/java/com/hbm/blocks/ModBlocks.java
@@ -1107,6 +1107,7 @@ public class ModBlocks {
 	public static Block machine_turbinegas;
 	public static Block machine_lpw2;
 	public static Block machine_htr3;
+	public static Block machine_htrf4;
 	public static Block machine_xenon_thruster;
 
 	public static Block press_preheater;
@@ -2407,6 +2408,7 @@ public class ModBlocks {
 		machine_turbinegas = new MachineTurbineGas(Material.iron).setBlockName("machine_turbinegas").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":block_steel");
 		machine_lpw2 = new MachineLPW2().setBlockName("machine_lpw2").setHardness(5.0F).setResistance(100.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":block_steel");
 		machine_htr3 = new MachineHTR3().setBlockName("machine_htr3").setHardness(5.0F).setResistance(100.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":block_steel");
+		machine_htrf4 = new MachineHTRF4().setBlockName("machine_htrf4").setHardness(5.0F).setResistance(100.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":block_steel");
 		machine_xenon_thruster = new MachineXenonThruster(Material.iron).setBlockName("machine_xenon_thruster").setHardness(5.0F).setResistance(100.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":block_steel");
 		press_preheater = new BlockBase(Material.iron).setBlockName("press_preheater").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":press_preheater");
 		machine_press = new MachinePress(Material.iron).setBlockName("machine_press").setHardness(5.0F).setResistance(10.0F).setCreativeTab(MainRegistry.machineTab).setBlockTextureName(RefStrings.MODID + ":machine_press");
@@ -3602,6 +3604,7 @@ public class ModBlocks {
 		register(machine_turbinegas);
 		register(machine_lpw2);
 		register(machine_htr3);
+		register(machine_htrf4);
 		register(machine_xenon_thruster);
 		GameRegistry.registerBlock(machine_schrabidium_transmutator, machine_schrabidium_transmutator.getUnlocalizedName());
 		GameRegistry.registerBlock(machine_discharger, machine_discharger.getUnlocalizedName());
