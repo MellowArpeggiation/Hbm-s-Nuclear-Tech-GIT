57b409c1a05a6061d226836db38ec9ee3ecec582
George Paton <georgewpaton@me.com>
2024-11-27 13:36:26 +1100

    machine hitbox and fullbright energy beam

--- src/main/java/com/hbm/blocks/machine/MachineDysonReceiver.java
+++ src/main/java/com/hbm/blocks/machine/MachineDysonReceiver.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.hbm.blocks.BlockDummyable;
 import com.hbm.blocks.ILookOverlay;
+import com.hbm.handler.MultiblockHandlerXR;
 import com.hbm.tileentity.TileEntityProxyCombo;
 import com.hbm.tileentity.machine.TileEntityDysonReceiver;
 import com.hbm.util.BobMathUtil;
@@ -14,6 +15,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.Pre;
+import net.minecraftforge.common.util.ForgeDirection;
 
 public class MachineDysonReceiver extends BlockDummyable implements ILookOverlay {
 	
@@ -36,6 +38,24 @@ public class MachineDysonReceiver extends BlockDummyable implements ILookOverlay
 	@Override
 	public int getOffset() {
 		return 2;
+	}
+
+	@Override
+	public void fillSpace(World world, int x, int y, int z, ForgeDirection dir, int o) {
+		super.fillSpace(world, x, y, z, dir, o);
+
+		x += dir.offsetX * o;
+		z += dir.offsetZ * o;
+
+		// Main structure
+		MultiblockHandlerXR.fillSpace(world, x, y, z, new int[] {1, 0, 8, -4, 1, 1}, this, dir);
+		MultiblockHandlerXR.fillSpace(world, x, y, z, new int[] {3, 0, 4, 0, 1, 1}, this, dir);
+
+		// Dish
+		MultiblockHandlerXR.fillSpace(world, x, y + 10, z, new int[] {1, 0, 6, 6, 6, 6}, this, dir);
+
+		// Tower
+		MultiblockHandlerXR.fillSpace(world, x, y, z, new int[] {17, 0, 1, 1, 1, 1}, this, dir);
 	}
 
 	@Override
--- src/main/java/com/hbm/render/tileentity/RenderDysonReceiver.java
+++ src/main/java/com/hbm/render/tileentity/RenderDysonReceiver.java
@@ -9,6 +9,7 @@ import com.hbm.render.util.BeamPronter.EnumBeamType;
 import com.hbm.render.util.BeamPronter.EnumWaveType;
 import com.hbm.tileentity.machine.TileEntityDysonReceiver;
 
+import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Vec3;
@@ -62,8 +63,16 @@ public class RenderDysonReceiver extends TileEntitySpecialRenderer {
 			int color = 0xff8800;
 
 			if(receiver.swarmCount > 0) {
+				GL11.glPushAttrib(GL11.GL_LIGHTING_BIT);
+				GL11.glDisable(GL11.GL_LIGHTING);
+
+				OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240F, 240F);
+				
 				BeamPronter.prontBeamwithDepth(Vec3.createVectorHelper(0, 0, length + 1), EnumWaveType.SPIRAL, EnumBeamType.SOLID, color, color, 0, 1, 0F, 2, 0.4F, 0.5F);
 				BeamPronter.prontBeamwithDepth(Vec3.createVectorHelper(0, 0, length + 1), EnumWaveType.RANDOM, EnumBeamType.SOLID, color, color, (int)(tileEntity.getWorldObj().getTotalWorldTime() % 1000), (length / 2) + 1, 0.0625F, 2, 0.4F, 0.5F);
+				
+				GL11.glEnable(GL11.GL_LIGHTING);
+				GL11.glPopAttrib();
 			}
 
 			GL11.glShadeModel(GL11.GL_FLAT);
