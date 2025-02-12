0a7f225d5990c8b7d8db956731fed258d236ce8e
George Paton <georgewpaton@me.com>
2025-02-12 14:02:33 +1100

    enable anatmogenesis configuration (gas type, add/remove via screwdriver)

--- src/main/java/com/hbm/blocks/machine/MachineDysonConverterAnatmogenesis.java
+++ src/main/java/com/hbm/blocks/machine/MachineDysonConverterAnatmogenesis.java
@@ -7,17 +7,27 @@ import com.hbm.blocks.BlockDummyable;
 import com.hbm.blocks.ILookOverlay;
 import com.hbm.dim.CelestialBody;
 import com.hbm.dim.trait.CBT_Atmosphere;
+import com.hbm.inventory.fluid.FluidType;
+import com.hbm.inventory.fluid.trait.FT_Gaseous;
+import com.hbm.inventory.fluid.trait.FluidTraitSimple.FT_Gaseous_ART;
+import com.hbm.items.machine.IItemFluidIdentifier;
 import com.hbm.tileentity.TileEntityProxyCombo;
 import com.hbm.tileentity.machine.TileEntityDysonConverterAnatmogenesis;
 import com.hbm.util.AstronomyUtil;
 import com.hbm.util.I18nUtil;
 
+import api.hbm.block.IToolable;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.ChatStyle;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.Pre;
 
-public class MachineDysonConverterAnatmogenesis extends BlockDummyable implements ILookOverlay {
+public class MachineDysonConverterAnatmogenesis extends BlockDummyable implements ILookOverlay, IToolable {
 
 	public MachineDysonConverterAnatmogenesis(Material mat) {
 		super(mat);
@@ -64,6 +74,56 @@ public class MachineDysonConverterAnatmogenesis extends BlockDummyable implement
 		text.add("Current mode: " + (converter.isEmitting ? "EMITTING" : "CAPTURING"));
 
 		ILookOverlay.printGeneric(event, I18nUtil.resolveKey(getUnlocalizedName() + ".name"), 0xffff00, 0x404000, text);
+	}
+
+	@Override
+	public boolean onScrew(World world, EntityPlayer player, int x, int y, int z, int side, float fX, float fY, float fZ, ToolType tool) {
+		if(tool != ToolType.SCREWDRIVER) return false;
+
+		if(world.isRemote) return true;
+
+		int[] pos = this.findCore(world, x, y, z);
+
+		if(pos == null) return false;
+
+		TileEntity te = world.getTileEntity(pos[0], pos[1], pos[2]);
+
+		if(!(te instanceof TileEntityDysonConverterAnatmogenesis)) return false;
+
+		TileEntityDysonConverterAnatmogenesis converter = (TileEntityDysonConverterAnatmogenesis) te;
+		converter.isEmitting = !converter.isEmitting;
+		converter.markDirty();
+
+		return true;
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+		if(world.isRemote)
+			return true;
+
+		if(player.getHeldItem() != null && player.getHeldItem().getItem() instanceof IItemFluidIdentifier) {
+			int[] pos = this.findCore(world, x, y, z);
+
+			if(pos == null) return false;
+
+			TileEntity te = world.getTileEntity(pos[0], pos[1], pos[2]);
+
+			if(!(te instanceof TileEntityDysonConverterAnatmogenesis))
+				return false;
+
+			TileEntityDysonConverterAnatmogenesis converter = (TileEntityDysonConverterAnatmogenesis) te;
+			FluidType type = ((IItemFluidIdentifier) player.getHeldItem().getItem()).getType(world, x, y, z, player.getHeldItem());
+			if(type.hasTrait(FT_Gaseous.class) || type.hasTrait(FT_Gaseous_ART.class)) {
+				converter.fluid = type;
+				converter.markDirty();
+				player.addChatComponentMessage(new ChatComponentText("Changed type to ").setChatStyle(new ChatStyle().setColor(EnumChatFormatting.YELLOW)).appendSibling(new ChatComponentTranslation(type.getConditionalName())).appendSibling(new ChatComponentText("!")));
+			}
+
+			return true;
+		}
+
+		return false;
 	}
 
 }
