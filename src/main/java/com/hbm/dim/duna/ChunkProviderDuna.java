a002547cc7fca05d51a82fc2378238df10335efc
George Paton <georgewpaton@me.com>
2025-03-09 16:40:50 +1100

    optimize duna cave gen

--- src/main/java/com/hbm/dim/duna/ChunkProviderDuna.java
+++ src/main/java/com/hbm/dim/duna/ChunkProviderDuna.java
@@ -6,32 +6,41 @@ import com.hbm.dim.duna.biome.BiomeGenBaseDuna;
 import com.hbm.dim.mapgen.ExperimentalCaveGenerator;
 
 import net.minecraft.world.World;
+
 public class ChunkProviderDuna extends ChunkProviderCelestial {
 
 	private ExperimentalCaveGenerator caveGenSmall = new ExperimentalCaveGenerator(2, 12, 0.12F);
 	private ExperimentalCaveGenerator caveGenV2 = new ExperimentalCaveGenerator(2, 40, 3.0F);
 
-    public ChunkProviderDuna(World world, long seed, boolean hasMapFeatures) {
-        super(world, seed, hasMapFeatures);
+	public ChunkProviderDuna(World world, long seed, boolean hasMapFeatures) {
+		super(world, seed, hasMapFeatures);
 		stoneBlock = ModBlocks.duna_rock;
 
-        caveGenV2.lavaBlock = ModBlocks.basalt;
-        caveGenV2.stoneBlock = ModBlocks.duna_rock;
-        caveGenSmall.lavaBlock = ModBlocks.duna_sands;
-        caveGenSmall.stoneBlock = ModBlocks.duna_rock;
+		caveGenV2.lavaBlock = ModBlocks.basalt;
+		caveGenV2.stoneBlock = ModBlocks.duna_rock;
+		caveGenSmall.lavaBlock = ModBlocks.duna_sands;
+		caveGenSmall.stoneBlock = ModBlocks.duna_rock;
 
-        caveGenSmall.smallCaveSize = 0.1F;
+		caveGenSmall.smallCaveSize = 0.1F;
 
-        caveGenV2.onlyBiome = BiomeGenBaseDuna.dunaLowlands;
-        caveGenSmall.ignoreBiome = BiomeGenBaseDuna.dunaLowlands;
-    }
+		caveGenV2.onlyBiome = BiomeGenBaseDuna.dunaLowlands;
+		caveGenSmall.ignoreBiome = BiomeGenBaseDuna.dunaLowlands;
+	}
 
-    @Override
+	@Override
 	public BlockMetaBuffer getChunkPrimer(int x, int z) {
 		BlockMetaBuffer buffer = super.getChunkPrimer(x, z);
 
-        this.caveGenV2.func_151539_a(this, worldObj, x, z, buffer.blocks);
-        this.caveGenSmall.func_151539_a(this, worldObj, x, z, buffer.blocks);
+		boolean hasLowlands = false;
+		boolean hasNotLowlands = false;
+
+		for(int i = 0; i < biomesForGeneration.length; i++) {
+			if(biomesForGeneration[i] == BiomeGenBaseDuna.dunaLowlands) hasLowlands = true; else hasNotLowlands = true;
+			if(hasLowlands && hasNotLowlands) break;
+		}
+
+		if(hasLowlands) this.caveGenV2.func_151539_a(this, worldObj, x, z, buffer.blocks);
+		if(hasNotLowlands) this.caveGenSmall.func_151539_a(this, worldObj, x, z, buffer.blocks);
 
 		return buffer;
 	}
