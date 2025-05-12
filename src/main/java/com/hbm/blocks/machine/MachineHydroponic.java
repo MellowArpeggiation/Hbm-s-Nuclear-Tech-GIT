18414d6265eb21887c2555d01bf15096c08d17b3
George Paton <georgewpaton@me.com>
2025-05-12 11:50:33 +1000

    convert CO2 to O2 while growing

--- src/main/java/com/hbm/blocks/machine/MachineHydroponic.java
+++ src/main/java/com/hbm/blocks/machine/MachineHydroponic.java
@@ -1,18 +1,27 @@
 package com.hbm.blocks.machine;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.hbm.blocks.BlockDummyable;
+import com.hbm.blocks.ILookOverlay;
 import com.hbm.handler.MultiblockHandlerXR;
 import com.hbm.tileentity.TileEntityProxyCombo;
 import com.hbm.tileentity.machine.TileEntityHydroponic;
+import com.hbm.util.I18nUtil;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.RenderGameOverlayEvent.Pre;
 import net.minecraftforge.common.IPlantable;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class MachineHydroponic extends BlockDummyable {
+public class MachineHydroponic extends BlockDummyable implements ILookOverlay {
 
 	public MachineHydroponic(Material mat) {
 		super(mat);
@@ -52,11 +61,45 @@ public class MachineHydroponic extends BlockDummyable {
 		// Sides
 		MultiblockHandlerXR.fillSpace(world, x, y, z, new int[] {2, 0, 0, 0, -2, 2}, this, dir);
 		MultiblockHandlerXR.fillSpace(world, x, y, z, new int[] {2, 0, 0, 0, 2, -2}, this, dir);
+
+		makeExtra(world, x + rot.offsetX * 2, y, z + rot.offsetZ * 2);
+		makeExtra(world, x - rot.offsetX * 2, y, z - rot.offsetZ * 2);
 	}
 
+	// Only the blocks immediately horizontally adjacent to the core can actually sustain plants, so we do a 4 block search in all cardinals
 	@Override
 	public boolean canSustainPlant(IBlockAccess world, int x, int y, int z, ForgeDirection direction, IPlantable plantable) {
-		return true;
+		if(world.getBlockMetadata(x, y, z) >= 12
+		|| world.getBlockMetadata(x + 1, y, z) >= 12
+		|| world.getBlockMetadata(x - 1, y, z) >= 12
+		|| world.getBlockMetadata(x, y, z + 1) >= 12
+		|| world.getBlockMetadata(x, y, z - 1) >= 12)
+			return true;
+		return false;
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void printHook(Pre event, World world, int x, int y, int z) {
+
+		int[] pos = this.findCore(world, x, y, z);
+
+		if(pos == null)
+			return;
+
+		TileEntity te = world.getTileEntity(pos[0], pos[1], pos[2]);
+
+		if(!(te instanceof TileEntityHydroponic))
+			return;
+
+		TileEntityHydroponic hydro = (TileEntityHydroponic) te;
+
+		List<String> text = new ArrayList<String>();
+
+		text.add(EnumChatFormatting.GREEN + "-> " + EnumChatFormatting.RESET + hydro.tanks[0].getTankType().getLocalizedName() + ": " + hydro.tanks[0].getFill() + "/" + hydro.tanks[0].getMaxFill() + "mB");
+		text.add(EnumChatFormatting.RED + "<- " + EnumChatFormatting.RESET + hydro.tanks[1].getTankType().getLocalizedName() + ": " + hydro.tanks[1].getFill() + "/" + hydro.tanks[1].getMaxFill() + "mB");
+
+		ILookOverlay.printGeneric(event, I18nUtil.resolveKey(getUnlocalizedName() + ".name"), 0xffff00, 0x404000, text);
 	}
 
 	// TEMP
--- src/main/java/com/hbm/tileentity/machine/TileEntityHydroponic.java
+++ src/main/java/com/hbm/tileentity/machine/TileEntityHydroponic.java
@@ -4,15 +4,21 @@ import com.hbm.blocks.BlockDummyable;
 import com.hbm.inventory.fluid.Fluids;
 import com.hbm.inventory.fluid.tank.FluidTank;
 import com.hbm.tileentity.TileEntityMachineBase;
+import com.hbm.util.fauxpointtwelve.DirPos;
 
+import api.hbm.fluidmk2.IFluidStandardTransceiverMK2;
+import io.netty.buffer.ByteBuf;
 import net.minecraft.block.Block;
 import net.minecraft.block.IGrowable;
 import net.minecraft.init.Blocks;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class TileEntityHydroponic extends TileEntityMachineBase {
+public class TileEntityHydroponic extends TileEntityMachineBase implements IFluidStandardTransceiverMK2 {
 
-	FluidTank[] tanks;
+	public FluidTank[] tanks;
+
+	private int[] prevMeta = new int[3];
 
 	public TileEntityHydroponic() {
 		super(4);
@@ -32,38 +38,105 @@ public class TileEntityHydroponic extends TileEntityMachineBase {
 			ForgeDirection dir = ForgeDirection.getOrientation(this.getBlockMetadata() - BlockDummyable.offset);
 			ForgeDirection rot = dir.getRotation(ForgeDirection.UP);
 
+			for(DirPos pos : getConPos()) {
+				trySubscribe(tanks[0].getTankType(), worldObj, pos);
+				tryProvide(tanks[1].getTankType(), worldObj, pos);
+			}
+
 			Block testPlant = Blocks.wheat;
 			IGrowable testGrow = (IGrowable) Blocks.wheat;
 
-			for(int i = -1; i <= 1; i++) {
-				int x = xCoord + rot.offsetX * i;
+			for(int i = 0; i < 3; i++) {
+				int x = xCoord + rot.offsetX * (i - 1);
 				int y = yCoord + 1;
-				int z = zCoord + rot.offsetZ * i;
+				int z = zCoord + rot.offsetZ * (i - 1);
 
 				Block currentPlant = worldObj.getBlock(x, y, z);
 
-				if(currentPlant != testPlant) {
-					worldObj.setBlock(x, y, z, testPlant);
-				}
-
-				testPlant.updateTick(worldObj, x, y, z, worldObj.rand);
-
-				boolean bonemeal = false;
-				if(testGrow.func_149851_a(worldObj, x, y, z, worldObj.isRemote)) { // should consume bonemeal, if not, assume fully grown
-					if(bonemeal) {
-						if(testGrow.func_149852_a(worldObj, worldObj.rand, x, y, z)) { // does bonemeal apply
-							testGrow.func_149853_b(worldObj, worldObj.rand, x, y, z); // apply bonemeal
-						}
-
-						// now consume the bonemeal
+				// Minimum CO2 pressure required to start growing
+				if(tanks[0].getFill() >= 100) {
+					if(currentPlant != testPlant) {
+						worldObj.setBlock(x, y, z, testPlant);
+						prevMeta[i] = worldObj.getBlockMetadata(x, y, z);
 					}
-				} else {
-					worldObj.setBlockToAir(x, y, z);
+
+					// a 3/(16^3) chance of ticking, multiplied by 10
+					// if(worldObj.rand.nextInt(136) == 0) testPlant.updateTick(worldObj, x, y, z, worldObj.rand);
+					if(worldObj.rand.nextInt(20) == 0) testPlant.updateTick(worldObj, x, y, z, worldObj.rand);
+
+					boolean fullyGrown = false;
+					boolean bonemeal = false;
+					if(testGrow.func_149851_a(worldObj, x, y, z, worldObj.isRemote)) { // should consume bonemeal, if not, assume fully grown
+						if(bonemeal) {
+							if(testGrow.func_149852_a(worldObj, worldObj.rand, x, y, z)) { // does bonemeal apply
+								testGrow.func_149853_b(worldObj, worldObj.rand, x, y, z); // apply bonemeal
+							}
+
+							// now consume the bonemeal
+						}
+					} else {
+						fullyGrown = true;
+					}
+
+					int newMeta = worldObj.getBlockMetadata(x, y, z);
+
+					if(newMeta != prevMeta[i]) {
+						// each growth stage sequesters 5mb of carbon
+						int toProduce = Math.max(newMeta - prevMeta[i], 0) * 5;
+						tanks[0].setFill(Math.max(tanks[0].getFill() - toProduce, 0));
+						tanks[1].setFill(Math.min(tanks[1].getFill() + toProduce, tanks[1].getMaxFill()));
+
+						prevMeta[i] = newMeta;
+					}
+
+					// after collecting produced O2, break any fully grown plants
+					if(fullyGrown) worldObj.setBlockToAir(x, y, z);
+				} else if(currentPlant instanceof IGrowable) {
+					// pause growth until sufficient CO2 added
+					worldObj.setBlockMetadataWithNotify(x, y, z, prevMeta[i], 2);
 				}
 			}
+
+			networkPackNT(15);
 		} else {
 
 		}
 	}
+
+	private DirPos[] getConPos() {
+		ForgeDirection dir = ForgeDirection.getOrientation(this.getBlockMetadata() - BlockDummyable.offset);
+		ForgeDirection rot = dir.getRotation(ForgeDirection.UP);
+
+		return new DirPos[] {
+			new DirPos(xCoord + rot.offsetX * 3, yCoord, zCoord + rot.offsetZ * 3, rot.getOpposite()),
+			new DirPos(xCoord - rot.offsetX * 3, yCoord, zCoord - rot.offsetZ * 3, rot),
+		};
+	}
+
+	@Override
+	public void serialize(ByteBuf buf) {
+		for(int i = 0; i < tanks.length; i++) tanks[i].serialize(buf);
+	}
+
+	@Override
+	public void deserialize(ByteBuf buf) {
+		for(int i = 0; i < tanks.length; i++) tanks[i].deserialize(buf);
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound nbt) {
+		super.writeToNBT(nbt);
+		for(int i = 0; i < tanks.length; i++) tanks[i].writeToNBT(nbt, "t" + i);
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound nbt) {
+		super.readFromNBT(nbt);
+		for(int i = 0; i < tanks.length; i++) tanks[i].readFromNBT(nbt, "t" + i);
+	}
+
+	@Override public FluidTank[] getReceivingTanks() { return new FluidTank[] { tanks[0] }; }
+	@Override public FluidTank[] getSendingTanks() { return new FluidTank[] { tanks[1] }; }
+	@Override public FluidTank[] getAllTanks() { return tanks; }
 
 }
