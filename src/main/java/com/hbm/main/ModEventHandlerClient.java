f28e24a2fab9eed04756cc90851cadeff422ea9c
George Paton <georgewpaton@me.com>
2025-07-26 14:50:20 +1000

    fix #300 - add config for ore location tooltip

--- src/main/java/com/hbm/main/ModEventHandlerClient.java
+++ src/main/java/com/hbm/main/ModEventHandlerClient.java
@@ -828,15 +828,17 @@ public class ModEventHandlerClient {
 		}*/
 
 		/// ORES ///
-		Block block = stack != null ? Block.getBlockFromItem(stack.getItem()) : null;
-		if(block instanceof net.minecraft.block.BlockOre || block instanceof BlockRedstoneOre) {
-			BlockOre ore = BlockOre.vanillaMap.get(block);
-			if(ore != null) {
-				ore.addInformation(stack, event.entityPlayer, list, event.showAdvancedItemTooltips);
-			} else if(block == Blocks.coal_ore) {
-				// we don't have any celestial coal, special case
-				list.add(EnumChatFormatting.GOLD + "Can be found on:");
-				list.add(EnumChatFormatting.AQUA + " - " + I18nUtil.resolveKey("body.kerbin"));
+		if(SpaceConfig.showOreLocations) {
+			Block block = stack != null ? Block.getBlockFromItem(stack.getItem()) : null;
+			if(block instanceof net.minecraft.block.BlockOre || block instanceof BlockRedstoneOre) {
+				BlockOre ore = BlockOre.vanillaMap.get(block);
+				if(ore != null) {
+					ore.addInformation(stack, event.entityPlayer, list, event.showAdvancedItemTooltips);
+				} else if(block == Blocks.coal_ore) {
+					// we don't have any celestial coal, special case
+					list.add(EnumChatFormatting.GOLD + "Can be found on:");
+					list.add(EnumChatFormatting.AQUA + " - " + I18nUtil.resolveKey("body.kerbin"));
+				}
 			}
 		}
 	}
@@ -1136,7 +1138,7 @@ public class ModEventHandlerClient {
 		Minecraft mc = Minecraft.getMinecraft();
 		long millis = Clock.get_ms();
 		if(millis == 0) millis = System.currentTimeMillis();
-		
+
 		if(GeneralConfig.enableLoadScreenReplacement && loadingScreenReplacementRetry < 25 && !(mc.loadingScreen instanceof LoadingScreenRendererNT) && millis > lastLoadScreenReplacement + 5_000) {
 			mc.loadingScreen = new LoadingScreenRendererNT(mc);
 			lastLoadScreenReplacement = millis;
