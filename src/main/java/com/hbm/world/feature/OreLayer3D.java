d2c810e47d1c94ba43ddccdc14970c4c341ddd0e
George Paton <georgewpaton@me.com>
2025-11-15 13:59:12 +1100

    improve orelayer gen performance when combined with RTG (which spams the decorate event :( )

--- src/main/java/com/hbm/world/feature/OreLayer3D.java
+++ src/main/java/com/hbm/world/feature/OreLayer3D.java
@@ -1,35 +1,43 @@
 package com.hbm.world.feature;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 import com.hbm.dim.WorldProviderCelestial;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.NoiseGeneratorPerlin;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
+import net.minecraftforge.event.world.WorldEvent;
 
 public class OreLayer3D {
-	
+
 	public static int counter = 0;
 	public int id;
 
 	NoiseGeneratorPerlin noiseX;
 	NoiseGeneratorPerlin noiseY;
 	NoiseGeneratorPerlin noiseZ;
-	
+
 	double scaleH;
 	double scaleV;
 	double threshold;
-	
+
 	Block block;
 	int meta;
 	int dim = 0;
 	boolean allCelestials = false;
-	
+
+	Map<Integer, Set<ChunkCoordIntPair>> alreadyDecorated = new HashMap<>();
+
 	public OreLayer3D(Block block, int meta) {
 		this.block = block;
 		this.meta = meta;
@@ -37,7 +45,7 @@ public class OreLayer3D {
 		this.id = counter;
 		counter++;
 	}
-	
+
 	public OreLayer3D setDimension(int dim) {
 		this.dim = dim;
 		return this;
@@ -48,17 +56,17 @@ public class OreLayer3D {
 		this.allCelestials = value;
 		return this;
 	}
-	
+
 	public OreLayer3D setScaleH(double scale) {
 		this.scaleH = scale;
 		return this;
 	}
-	
+
 	public OreLayer3D setScaleV(double scale) {
 		this.scaleV = scale;
 		return this;
 	}
-	
+
 	public OreLayer3D setThreshold(double threshold) {
 		this.threshold = threshold;
 		return this;
@@ -66,9 +74,10 @@ public class OreLayer3D {
 
 	@SubscribeEvent
 	public void onDecorate(DecorateBiomeEvent.Pre event) {
-
 		World world = event.world;
-		
+		int cX = event.chunkX;
+		int cZ = event.chunkZ;
+
 		if(world.provider == null) return;
 
 		Block replace = Blocks.stone;
@@ -82,23 +91,27 @@ public class OreLayer3D {
 			if(world.provider.dimensionId != this.dim) return;
 		}
 
+		ChunkCoordIntPair chunkPos = new ChunkCoordIntPair(cX, cZ);
+		Set<ChunkCoordIntPair> decoratedChunks = alreadyDecorated.computeIfAbsent(world.provider.dimensionId, n -> new HashSet<>());
+
+		// Stop early if we've already generated this chunk in this dimension
+		if(decoratedChunks.contains(chunkPos)) return;
+		decoratedChunks.add(chunkPos);
+
 		if(this.noiseX == null) this.noiseX = new NoiseGeneratorPerlin(new Random(event.world.getSeed() + 101 + id), 4);
 		if(this.noiseY == null) this.noiseY = new NoiseGeneratorPerlin(new Random(event.world.getSeed() + 102 + id), 4);
 		if(this.noiseZ == null) this.noiseZ = new NoiseGeneratorPerlin(new Random(event.world.getSeed() + 103 + id), 4);
-		
-		int cX = event.chunkX;
-		int cZ = event.chunkZ;
-		
+
 		for(int x = cX + 8; x < cX + 24; x++) {
 			for(int z = cZ + 8; z < cZ + 24; z++) {
 				for(int y = 64; y > 5; y--) {
 					double nX = this.noiseX.func_151601_a(y * scaleV, z * scaleH);
 					double nY = this.noiseY.func_151601_a(x * scaleH, z * scaleH);
 					double nZ = this.noiseZ.func_151601_a(x * scaleH, y * scaleV);
-					
+
 					if(nX * nY * nZ > threshold) {
 						Block target = world.getBlock(x, y, z);
-						
+
 						if(target.isNormalCube() && target.isReplaceableOreGen(world, x, y, z, replace)) {
 							world.setBlock(x, y, z, block, meta, 2);
 						}
@@ -107,4 +120,15 @@ public class OreLayer3D {
 			}
 		}
 	}
+
+	@SubscribeEvent
+	public void onWorldLoad(WorldEvent.Load event) {
+		alreadyDecorated.put(event.world.provider.dimensionId, new HashSet<>());
+	}
+
+	@SubscribeEvent
+	public void onWorldUnload(WorldEvent.Unload event) {
+		alreadyDecorated.remove(event.world.provider.dimensionId);
+	}
+
 }
