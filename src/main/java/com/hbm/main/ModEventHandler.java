a11fec4b2fbe086432f10b40dfc683fef5c30eb9
George Paton <georgewpaton@me.com>
2025-12-02 17:05:54 +1100

    hostile takeover

--- src/main/java/com/hbm/main/ModEventHandler.java
+++ src/main/java/com/hbm/main/ModEventHandler.java
@@ -191,7 +191,7 @@ public class ModEventHandler {
 		if(!event.player.worldObj.isRemote) {
 
 			if(GeneralConfig.enableMOTD) {
-				event.player.addChatMessage(new ChatComponentText("Loaded world with JameH2's NTM: Space " + RefStrings.VERSION + " for Minecraft 1.7.10!"));
+				event.player.addChatMessage(new ChatComponentText("Loaded world with JamesH2 & Mellow's NTM: Space " + RefStrings.VERSION + " for Minecraft 1.7.10!"));
 
 				if(HTTPHandler.newVersion) {
 					event.player.addChatMessage(
